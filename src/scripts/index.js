81c1b66921f9806d8fc9e9083d3515cbac9d7743
DK <i386.net@gmail.com>
2020-08-19 12:25:22 +0300

    update: webpack config

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,39 +1,45 @@
-const copyright = document.querySelector('.footer__copyright-container');
-const loginElement = document.querySelector('.popup_type_login');
-const signupElement = document.querySelector('.popup_type_reg');
-const signedupElement = document.querySelector('.popup_type_reged');
-const authButton = document.querySelector('#signin');
-const registerButton = document.querySelector('.popup__link_type_reg');
-const loginButton = document.querySelector('.popup__link_type_login');
-const regButton = document.querySelector('#register');
+import Popup from './classes/Popup';
+import '../styles/index.css';
 
-const loginPopup = new Popup(loginElement);
-const signupPopup = new Popup(signupElement);
-const signedupPopup = new Popup(signedupElement);
+(function () {
+  const copyright = document.querySelector('.footer__copyright-container');
+  const loginElement = document.querySelector('.popup_type_login');
+  const signupElement = document.querySelector('.popup_type_reg');
+  const signedupElement = document.querySelector('.popup_type_reged');
+  const authButton = document.querySelector('#signin');
+  const registerButton = document.querySelector('.popup__link_type_reg');
+  const loginButton = document.querySelector('.popup__link_type_login');
+  const regButton = document.querySelector('#register');
+
+  const loginPopup = new Popup(loginElement);
+  const signupPopup = new Popup(signupElement);
+  const signedupPopup = new Popup(signedupElement);
 
 
-authButton.addEventListener('click', ((evt) => {
-  evt.preventDefault();
-  loginPopup.open();
-}));
-registerButton.addEventListener('click', (evt) => {
-  evt.preventDefault();
-  loginPopup.close(evt);
-  signupPopup.open();
-})
-loginButton.addEventListener('click', (evt => {
-  evt.preventDefault();
-  signupPopup.close(evt);
-  loginPopup.open();
-}))
-regButton.addEventListener('click', evt => {
-  console.log(evt);
-  evt.preventDefault();
-  signupPopup.close(evt);
-  signedupPopup.open();
+  authButton.addEventListener('click', ((evt) => {
+    evt.preventDefault();
+    loginPopup.open();
+  }));
+  registerButton.addEventListener('click', (evt) => {
+    evt.preventDefault();
+    loginPopup.close(evt);
+    signupPopup.open();
+  })
+  loginButton.addEventListener('click', (evt => {
+    evt.preventDefault();
+    signupPopup.close(evt);
+    loginPopup.open();
+  }))
+  regButton.addEventListener('click', evt => {
+    console.log(evt);
+    evt.preventDefault();
+    signupPopup.close(evt);
+    signedupPopup.open();
 
-})
+  })
 
-alert('hello');
+  alert('hello');
 
-copyright.textContent = `© ${new Date().getFullYear()} Supersite, Powered by News AP`;
+  copyright.textContent = `© ${new Date().getFullYear()} Supersite, Powered by News AP`;
+
+})();
---
+++ webpack.config.js
@@ -0,0 +1,75 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WebpackMd5Hash = require('webpack-md5-hash');
+const CopyPlugin = require('copy-webpack-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const isDev = process.env.NODE_ENV === 'development';
+const webpack = require('webpack');
+
+module.exports = {
+  entry: { main: './src/scripts/index.js' },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[chunkhash].js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/i,
+        use: { loader: 'babel-loader' },
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/i,
+        use: [
+          isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
+          'css-loader',
+          'postcss-loader',
+        ],
+      },
+      {
+        test: /\.(png|jpe?g|gif|ico|svg)$/i,
+        use: [
+          'file-loader?name=./images/[name].[ext]',
+          {
+            loader: 'image-webpack-loader',
+            options: {},
+          },
+        ],
+      },
+      {
+        test: /\.(eot|ttf|woff|woff2)$/i,
+        loader: 'file-loader?name=./vendor/[name].[ext]',
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'index.[contenthash].css',
+    }),
+    new HtmlWebpackPlugin({
+      inject: false,
+      template: './src/index.html',
+      filename: 'index.html',
+    }),
+    new WebpackMd5Hash(),
+    new webpack.DefinePlugin({
+      NODE_ENV: JSON.stringify(process.env.NODE_ENV),
+    }),
+    new OptimizeCssAssetsPlugin({
+      assetNameRegExp: /\.css$/g,
+      cssProcessor: require('cssnano'),
+      cssProcessorPluginOptions: {
+        preset: ['default'],
+      },
+      canPrint: true,
+    }),
+    new CopyPlugin({
+      patterns: [{
+        from: 'src/images',
+        to: 'images'
+      }],
+    }),
+  ],
+};
