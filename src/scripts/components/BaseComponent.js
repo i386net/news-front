f8146c85fb3fb297e0ff3d60edf7217389b8f844
DK <i386.net@gmail.com>
2020-09-04 13:11:01 +0300

    update: css menu icon position

---
+++ src/scripts/components/BaseComponent.js
@@ -0,0 +1,22 @@
+export default class BaseComponent {
+  constructor() {
+    this._listeners = [];
+    this._addListener = this._addListener.bind(this);
+  }
+  _addListener({element, event, callback}) {
+    element.addEventListener(event, callback);
+  }
+  _setHandlers(listeners) {
+    listeners.forEach(listener => {
+      this._listeners.push(listener);
+      this._addListener({...listener});
+    });
+  }
+  _clearListeners() {
+    this._listeners.forEach((listener => {
+      const {element, event, callback} = listener;
+      element.removeEventListener(event, callback);
+    }))
+    this._listeners = [];
+  }
+}
