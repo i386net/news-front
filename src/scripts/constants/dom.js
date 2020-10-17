53c856feedcabe367fc2ac914d975631456e051c
DK <i386.net@gmail.com>
2020-10-17 11:09:40 +0300

    add: DOM constants

---
+++ src/scripts/constants/dom.js
@@ -0,0 +1,12 @@
+export default {
+  header: document.querySelector('.header'),
+  headerArea: document.querySelector('.header-area'),
+  authButton: document.querySelector('.nav__button'),
+  burgerButton: document.querySelector('.burger-button'),
+  signupPopup: document.querySelector('.popup_type_signup'),
+  signinPopup: document.querySelector('.popup_type_signin'),
+  successPopup: document.querySelector('.popup_type_registered'),
+  signupLink: document.querySelector('.popup__link_type_reg'),
+  signinLink: document.querySelector('.popup__link_type_login'),
+  copyright: document.querySelector('.footer__copyright-container'),
+}
