d9d6c1019b729c576d0b7bc566927518eb8735c6
DK <i386.net@gmail.com>
2020-10-25 01:32:25 +0300

    update: add theme to Header

--- src/scripts/components/Header.js
+++ src/scripts/components/Header.js
@@ -1,7 +1,7 @@
 import BaseComponent from './BaseComponent';
 
 export default class Header extends  BaseComponent {
-  constructor({ headerArea, popup, api, session }) {
+  constructor({ headerArea, popup, api, session, theme }) {
     super();
     this.headerArea = headerArea;
     this.popup = popup;
@@ -11,6 +11,7 @@ export default class Header extends  BaseComponent {
     this.mobileAuthButton = document.querySelector('.menu__button');
     this.isLoggedIn = null;
     this.name  = null;
+    this.theme = theme;
   }
 
 
@@ -40,7 +41,7 @@ export default class Header extends  BaseComponent {
     this.desktopButton.textContent = '';
     this.desktopButton.classList.add(
       'button',
-      'button_theme_dark',
+      `button_theme_${this.theme}`,
       'button_size_m',
       'nav__button'
     );
@@ -48,7 +49,7 @@ export default class Header extends  BaseComponent {
       'beforeend',
       `
       <span class="auth-text">${name? name : 'Авторизоваться'}</span>
-      <svg  class="button__ico ${name? '' : 'button__ico_is-hidden' }" width="24" height="24" viewBox="0 0 24 24" fill="#000" xmlns="http://www.w3.org/2000/svg">
+      <svg  class="button__ico button__ico_theme_${this.theme} ${name? '' : 'button__ico_is-hidden' }" width="24" height="24" viewBox="0 0 24 24" fill="#000" xmlns="http://www.w3.org/2000/svg">
         <path fill-rule="evenodd" clip-rule="evenodd" d="M10 6L6 6L6 18H10V20H6C4.89543 20 4 19.1046 4 18V6C4 4.89543 4.89543 4 6 4H10V6ZM17.5856 13L13.2927 17.1339L14.707 18.4958L21.4141 12.0371L14.707 5.57837L13.2927 6.9402L17.5856 11.0741H8V13H17.5856Z"/>
       </svg>
       `);
@@ -67,7 +68,6 @@ export default class Header extends  BaseComponent {
       this.mobileAuthButton
         .querySelector('.button__ico')
         .classList.add('button__ico_is-hidden');
-
     }
   }
 
@@ -75,7 +75,7 @@ export default class Header extends  BaseComponent {
     return {
       nav: `
         <li class="nav__item articles-link">
-            <a href="./articles.html" class="nav__link nav__link_theme_dark">Сохраненные статьи</a>
+            <a href="./articles.html" class="nav__link nav__link_theme_${this.theme}">Сохраненные статьи</a>
         </li>
       ` ,
       menu: `
@@ -99,9 +99,6 @@ export default class Header extends  BaseComponent {
             this.render(this.isLoggedIn);
             this.desktopButton.classList.remove('logged-in');
             this.mobileAuthButton.classList.remove('logged-in');
-            // if(!window.location.pathname === '/') {
-            //   window.location.href = '/';
-            // }
             window.location.href = '/';
           } else {
             return Promise.reject(new Error('При выходе произошла ошибка!'))
