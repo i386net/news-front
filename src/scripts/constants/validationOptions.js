d9aa6b5312734b4dc67171e0bd953e90b844499d
DK <i386.net@gmail.com>
2020-10-24 07:41:11 +0300

    add: email validation options

---
+++ src/scripts/constants/validationOptions.js
@@ -0,0 +1,9 @@
+export default {
+  email: {
+    require_tld: true,
+    allow_ip_domain: false,
+    allow_utf8_local_part: false,
+    ignore_max_length: false,
+    domain_specific_validation: true,
+  }
+}
